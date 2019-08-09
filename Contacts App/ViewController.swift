eaee8e90a6aa411f16cc06eefe501e2ab371016c
Yash Bedi <yash.bedi@e9ine.com>
2019-08-09 10:25:19 +0530

    Remove Main.storyboard, Add assets. Modify Launch.storyboard.

---
+++ Contacts App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Contacts App
+//
+//  Created by Yash Bedi on 06/08/19.
+//  Copyright © 2019 Yash Bedi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
