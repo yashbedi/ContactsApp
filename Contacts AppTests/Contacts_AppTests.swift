84a659680a80318ec7ebb92cabb029b50162abc9
Yash Bedi <yash.bedi@e9ine.com>
2019-08-10 20:12:13 +0530

    Complete problem statement.

--- Contacts AppTests/Contacts_AppTests.swift
+++ Contacts AppTests/Contacts_AppTests.swift
@@ -10,11 +10,11 @@ import XCTest
 @testable import Contacts_App
 
 class Contacts_AppTests: XCTestCase {
-
+    
     override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
     }
-
+    
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
